79944eae5edb1c9fc857d5ff6f47a0e235b626dd
Ryo Nakamine <rnakamine8080@gmail.com>
2021-03-28 21:59:13 +0900

    Update sample code

--- check_exist.py
+++ check_exist.py
@@ -7,8 +7,16 @@ connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
 
 def main():
     container_name = 'testnakamine'
+    blob_name = 'sample.txt'
+
     blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-    blob_service_client.create_container(container_name)
+    blob_client = blob_service_client.get_blob_client(
+        container_name, blob=blob_name)
+
+    if blob_client.exists():
+        print('Exists')
+    else:
+        print('Not exists')
 
 
 if __name__ == '__main__':
--- list_blobs.py
+++ list_blobs.py
@@ -8,7 +8,11 @@ connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
 def main():
     container_name = 'testnakamine'
     blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-    blob_service_client.create_container(container_name)
+    container_client = blob_service_client.get_container_client(container_name)
+
+    blobs = container_client.list_blobs()
+    for blob in blobs:
+        print(blob['name'], blob['last_modified'])
 
 
 if __name__ == '__main__':
